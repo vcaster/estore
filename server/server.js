942a6494df79366e5d6b17f86525b96bf318d918
vcaster <ainaniran@yahoo.com>
2019-10-26 14:06:01 -0400

    hashing paswords

--- server/server.js
+++ server/server.js
@@ -1,9 +1,47 @@
-const express = require('express');
+const express = require('express');// server
+const bodyParser = require('body-parser'); // brings json from actual request --check packe if installed
+const cookieParser = require('cookie-parser'); // for cookies
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE) // connnect to db
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//=================================
+//           MODELS
+//=================================
+
+const { User } = require('./models/user');
+
+//=================================
+//           USERS
+//=================================
+
+app.post('/api/users/register', (req,res) => {
+    
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success: false, err});
+        res.status(200).json({
+            success:true,
+            userdata:doc
+        })
+    })
+
+})
+
+
 
 const port = process.env.port || 3002;
 
 app.listen(port,() => {
     console.log(`server running at ${port}`)
 })
+

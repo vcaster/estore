47558b18c923d5e3e2111cd293e0683a5bb2728a
vcaster <ainaniran@yahoo.com>
2019-10-28 13:45:07 -0400

    login route

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
 const SALT_I = 10;
+
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email:{
@@ -66,6 +70,28 @@ userSchema.pre('save', function(next){
     }
     
 })
+
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET) // using tokens
+
+    user.token = token;
+
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
 
 const User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ const { User } = require('./models/user');
 //           USERS
 //=================================
 
+app.get('/api/users/auth' , (req,res) =>{
+    
+})
+
 app.post('/api/users/register', (req,res) => {
     
     const user = new User(req.body);
@@ -37,6 +41,26 @@ app.post('/api/users/register', (req,res) => {
 
 })
 
+app.post('/api/users/login',(req,res)=>{
+    
+    User.findOne({'email': req.body.email}, (err,user)=>{
+        if(!user) return res.json({loginSuccess:false, message:'Ath failed! email not founf'}); // checks for email if found
+
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false, message:'Wrong password'});
+
+            user.generateToken((err, user)=>{  
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+            
+            })
+        })
+
+    })
+
+})
 
 
 const port = process.env.port || 3002;

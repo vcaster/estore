47d8fea31f1a37ea9102b0b45dc824706059698a
vcaster <ainaniran@yahoo.com>
2019-10-30 12:19:39 -0400

    routes plain with routes

--- server/server.js
+++ server/server.js
@@ -127,7 +127,7 @@ app.post('/api/product/brand', auth,admin, (req,res) =>{
     })
 })
 
-app.get('/api/product/brands', auth,admin, (req,res) =>{
+app.get('/api/product/brands', (req,res) =>{
     Brand.find({},(err,brands)=>{
         if(err) return res.status(400).send(err);
         res.status(200).send(brands)

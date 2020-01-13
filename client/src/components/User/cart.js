80c7a5ad5f8b69c753e5e1b53a0af74e01720d4c
vcaster <ainaniran@yahoo.com>
2020-01-12 23:45:09 -0500

    Adding CART total Done

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import UserLayout from '../../hoc/user'
-
+import UserProductBlock from '../utils/User/product_block'
 import { connect } from 'react-redux'
 import { getCartItems} from '../../actions/user_actions'
 
@@ -28,17 +28,88 @@ class UserCart extends Component {
                 user.userData.cart.forEach(item =>{
                     cartItem.push(item.id)
                 });
-                this.props.dispatch(getCartItems(cartItem, user.userData.cart))
+                this.props.dispatch(getCartItems(cartItem, user.userData.cart)).then(()=>{
+                    if(this.props.user.cartDetail.length > 0){
+                        this.calculateTotal(this.props.user.cartDetail);
+                    }
+                })
 
             }            
         }
     }
 
+    calculateTotal = (cartDetail) => {
+        let total =0;
+
+        cartDetail.forEach(item =>{
+            total += parseInt(item.price, 10) * item.quantity
+        })
+        this.setState({
+            total,
+            showTotal: true
+        })
+    }
+
+    removeFromCart = () => {
+        
+    }
+
+    showNoItem = () => (
+        <div className="cart_no_items">
+        <FontAwesomeIcon icon={faFrown}/>
+        <div className="">
+            You have no Items
+        </div>
+         
+        </div>
+    )
+
     render() {
         return (
             <UserLayout>
                 <div>
-                    cart
+                    <h1>My cart</h1>
+                    <div className="user_cart">
+                        <UserProductBlock
+                            products={this.props.user}
+                            type="cart"
+                            removeItem={(id)=>this.removeFromCart(id)}
+                        />
+                        {this.state.showTotal ?
+
+                            <div className="">
+                                <div className="user_cart_sum">
+                                    <div className="">
+                                        Total amount: $ {this.state.total}
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        :
+                            this.state.showSuccess ?
+                                <div className="cart_success">
+                                <FontAwesomeIcon icon={faSmile}/>
+                                <div className="">
+                                    Thank you for your purchase
+                                </div>
+                                    
+                                </div>
+                            :
+
+                            this.showNoItem()
+                        }
+                    </div>
+
+
+                        {
+                            this.state.showTotal ?
+                            <div className="paypal_button_container">
+                                Paypal
+                            </div>
+                            :
+                            null
+                        }
+
                 </div>
             </UserLayout>
             
